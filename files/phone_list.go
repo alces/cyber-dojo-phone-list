78844aceba9608fe834821d72b83695dfc87dfbe
jfzLJV <jfzLJV@cyber-dojo.org>
2024-05-14 14:33:06 +0000

    9 ran tests, predicted green, got green

--- files/phone_list.go
+++ files/phone_list.go
@@ -1,9 +1,21 @@
 package phonelist
 
-func isConsistent(plist []string) bool {
-    if len(plist) <= 1 {
+import (
+    "strings"
+)
+
+func isConsistent(p []string) bool {
+    if len(p) <= 1 {
         return true
     }
     
-    return false
+    for i := 0; i < len(p) - 1; i++ {
+        for j := i + 1; j < len(p); j++ {
+            if strings.HasPrefix(p[j], p[i]) {
+                return false
+            }
+        }
+    }
+    
+    return true
 }
