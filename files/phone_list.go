37addea12b2277da58d83d541e9e6f0ef819120e
jfzLJV <jfzLJV@cyber-dojo.org>
2024-05-14 14:33:28 +0000

    10 ran tests, predicted green, got green

--- files/phone_list.go
+++ files/phone_list.go
@@ -5,10 +5,6 @@ import (
 )
 
 func isConsistent(p []string) bool {
-    if len(p) <= 1 {
-        return true
-    }
-    
     for i := 0; i < len(p) - 1; i++ {
         for j := i + 1; j < len(p); j++ {
             if strings.HasPrefix(p[j], p[i]) {
