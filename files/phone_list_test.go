9518df2d384be02420453ba83f9b9ab56fb002b7
jfzLJV <jfzLJV@cyber-dojo.org>
2024-05-14 14:34:15 +0000

    11 ran tests, predicted green, got green

--- files/phone_list_test.go
+++ files/phone_list_test.go
@@ -13,6 +13,11 @@ func TestOneElementListShouldBeConsistent(t *testing.T) {
     assert.True(t, isConsistent([]string{"911"}))
 }
 
+func TestListWithSimplePrefixMatches(t *testing.T) {
+    sample := []string{"911", "91101", "913"}
+    assert.False(t, isConsistent(sample), sample)
+}
+
 func TestListWithNoSimplePrefixMatches(t *testing.T) {
     sample := []string{"911", "912", "913"}
     assert.True(t, isConsistent(sample), sample)
