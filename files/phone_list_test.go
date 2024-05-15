f6784ad76d0324059129fdde7e11f42274fc06b6
jfzLJV <jfzLJV@cyber-dojo.org>
2024-05-15 12:51:43 +0000

    62 ran tests, predicted green, got green

--- files/phone_list_test.go
+++ files/phone_list_test.go
@@ -15,10 +15,10 @@ var phoneListTestResults = []struct {
     {[]string{}, true, "empty list should be consistent"},
     {[]string{"911"}, true, "one-element list should be consistent"},
     {[]string{"911", "91101", "913"}, false, "a list with literal matches should be inconsistent"},
-    {[]string{"911", "912", "913"}, true, "a list with no matches should be consistent"},
-    {[]string{"911", "91-101", "913"}, false, "a list with matches after canonization should be inconsistent"},
-    {[]string{"91101", "911", "913"}, false, "an incosistent list in different order should be inconsistent"},
-    {[]string{"911", "912", "911"}, true, "duplicates shouldn't be considered incosistencies"},
+    {[]string{"911", "91245", "9130", "9145"}, true, "a list with no matches should be consistent"},
+    {[]string{"911", "91-101", "913", "9-14 55"}, false, "a list with matches after canonization should be inconsistent"},
+    {[]string{"91101", "912", "911", "913"}, false, "an incosistent list in different order should be inconsistent"},
+    {[]string{"911", "912", "911", "913"}, true, "duplicates shouldn't be considered incosistencies"},
 }
 
 func TestPhoneList(t *testing.T) {
