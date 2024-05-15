73dc329d7454af8acd7644e53e7edf051958ae14
jfzLJV <jfzLJV@cyber-dojo.org>
2024-05-15 09:37:51 +0000

    53 ran tests, predicted green, got amber

--- files/optimizer.go
+++ files/optimizer.go
@@ -1,5 +1,23 @@
 package phonelist
 
+import (
+    "sort"
+)
+
+type ByLen []string
+
+func (ByLen b) Len() int {
+    return len(b)
+}
+
+func (ByLen b) Swap(i, j int) {
+    b[i], b[j] = b[j], b[i]
+}
+
+func (ByLen b) Less(i, j int) bool {
+    return len(b[i]) < len(b[j])
+}
+
 func sortByLen(a []string) []string {
-    return a
+    return sort.Sort(ByLen(a))
 }
